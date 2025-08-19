ebb84d1c26d10649f5efa640103dd645dd0d292e
vanshikajain1708 <vanshijain1708@gmail.com>
2025-08-19 12:21:09 -0400

    Initial commit on java-fundamental branch

---
+++ 02 Java Programming Elements/HarryAge.java
@@ -0,0 +1,13 @@
+public class HarryAge {
+    public static void main(String[] args) {
+        // Given values
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculate age
+        int age = currentYear - birthYear;
+
+        // Output
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
